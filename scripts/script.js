85459dd78084f5bcf7f156c9c8702a1ffacfd4e5
USTower7 <78798982+USTower7@users.noreply.github.com>
2021-07-17 21:12:41 +0300

    Adaptiv slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,52 @@
-let offset = 0;
-let blockWidth = slider.scrollWidth;
-const sliderLine = document.querySelector('#sliderImg')
+let count = 0;
+let width;
+const sliderLine = document.querySelector('.slider__img')
+const images = document.querySelectorAll('.slider .slider__img img')
 
-next.addEventListener('click', function() {
-    let blockWidth = slider.scrollWidth;
 
-    
+function init() {
+    width = document.querySelector('.slider').offsetWidth
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
 
-    if(offset + 250 < blockWidth) {
-        offset += 250;
-        sliderLine.style.right = offset +'px';
-        console.log(offset)
-    } else {
-        offset = 0;
-        sliderLine.style.right = offset +'px';
+    })
+    rollSlider()
+}
+
+
+window.addEventListener('resize', init)
+init()
+
+next.addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
     }
-
-    console.log(blockWidth)
+    rollSlider()
+})
+prev.addEventListener('click', function () {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider()
 })
 
-prev.addEventListener('click', function() {
+/*prev.addEventListener('click', function () {
     let blockWidth = slider.scrollWidth;
 
-    
 
-    if(offset <= blockWidth && offset > 0) {
+
+    if (offset <= blockWidth && offset > 0) {
         offset -= 250;
-        sliderLine.style.right = offset +'px';
-        console.log(offset)
+        sliderLine.style.right = offset + 'px';
     } else {
         offset = blockWidth - 250;
-        sliderLine.style.right = offset +'px';
-        console.log(offset)
+        sliderLine.style.right = offset + 'px';
     }
 
-})
+})*/
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
