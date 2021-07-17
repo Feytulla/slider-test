4d1613850ce8835a9ebfc27c58035d7c4322033f
USTower7 <78798982+USTower7@users.noreply.github.com>
2021-07-17 22:01:56 +0300

    Adaptiv slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,36 @@
+const images = document.querySelectorAll(".slider .slider__line img");
+const sliderLine = document.querySelector('.slider__line')
 let count = 0;
 let width;
-const sliderLine = document.querySelector('.slider__img')
-const images = document.querySelectorAll('.slider .slider__img img')
-
 
 function init() {
-    width = document.querySelector('.slider').offsetWidth
-    images.forEach(item => {
-        item.style.width = width + 'px';
-        item.style.height = 'auto';
-
+    width = document.querySelector('.slider').offsetWidth;
+    images.forEach(img => {
+        img.style.width = width + 'px';
+        img.style.height = 'auto';
     })
-    rollSlider()
+    resizeLine()
 }
 
-
-window.addEventListener('resize', init)
+window.addEventListener('resize', init);
 init()
 
-next.addEventListener('click', function () {
+document.querySelector('.slider-next').addEventListener('click', function() {
     count++;
     if (count >= images.length) {
         count = 0;
     }
-    rollSlider()
-})
-prev.addEventListener('click', function () {
-    count--;
-    if (count < 0) {
-        count = images.length - 1;
-    }
-    rollSlider()
+    resizeLine()
 })
 
-function rollSlider() {
-    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+document.querySelector('.slider-prev').addEventListener('click', function() {
+    count--;
+    if (count < images.length) {
+        count = images.length - 1;
+    }
+    resizeLine()
+})
+
+function resizeLine() {
+    sliderLine.style.transform = 'translate(-' + count * width +'px)';
 }
